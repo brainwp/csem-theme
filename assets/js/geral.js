f2236589f12d55209b053a5611ee106efbfedb50
matheusgimenez <contato@matheusgimenez.com.br>
2019-02-07 15:45:55 -0200

    issue #2 - coloca em posição absoluta para possibilitar scroll

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -10,13 +10,19 @@ jQuery(document).ready(function($) {
 	 *
 	*/
 	$( '.csem-open-menu' ).on( 'click', function( e ) {
-		console.log( this )
+		e.preventDefault();
 		if ( $( this ).hasClass( 'open' ) ) {
 			$( this ).removeClass( 'open' );
 			$( '.menu-logo-toggle' ).removeClass( 'open' );
 		} else {
+			$( 'html, body' ).animate({
+        		scrollTop: 0
+        	}, 800); 
 			$( '.menu-logo-toggle' ).addClass( 'open' );
 			$( this ).addClass( 'open' ); 
 		}
+	});
+	$( window ).load( function() {
+		$( '.menu-logo-toggle' ).css( 'min-height', $( document ).height() + 'px' );
 	});
 });
